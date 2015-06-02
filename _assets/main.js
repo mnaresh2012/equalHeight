a896b153dd3f6233a3c44d21950085af80cd681f
naresh kumar <mnaresh2010@aol.com>
2015-06-02 16:29:22 +0530

    Update main.js

--- _assets/main.js
+++ _assets/main.js
@@ -1,1 +1,27 @@
+/*!
+* NAME OF THE FILE (main.js)
+* This file contains the code for the equalHeight.
+* 
+* @project   equalHeight
+* @date      02-06-2015 
+* @author    mr.naresh kumar <mnaresh2010@aol.com>
+* @license  MIT
+*
+*/
+
+'use strict'
+;(function ($) {
+	$.fn.equalHeight = function () {
+		var tallest = 0,
+		elements = this;
+		elements.each(function (index, elem) {
+			var maxHeight = $(elem).outerHeight();
+			if (maxHeight >= tallest) {
+				tallest = maxHeight;
+			}
+		});
+		elements.height(tallest);
+		return this;
+	}
+}(jQuery));
 
