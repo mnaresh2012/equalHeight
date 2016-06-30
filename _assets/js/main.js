073a31932e38e59692c5a1a788f84691fb810386
naresh kumar <mnaresh2010@aol.com>
2016-06-30 15:18:08 +0530

    Create main.js

---
+++ _assets/js/main.js
@@ -0,0 +1,18 @@
+/*!
+* NAME OF THE FILE (main.js)
+* In this file we are initializing the plug-in.
+* 
+* @project   NA
+* @date      030-06-2016
+* @author    mr.naresh kumar <mnaresh2010@aol.com>
+* @license  MIT
+*
+*/
+
+'use strict';
+
+$(function() {
+	//pugin initialization
+	$('.container ul li p').equalHeight();
+});
+
