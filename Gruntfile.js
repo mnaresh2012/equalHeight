d0ed2ae7d4376e35e4d1c41ee2975adaf9ab6bee
naresh kumar <mnaresh2010@aol.com>
2016-06-30 15:20:05 +0530

    Delete Gruntfile.js

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt){
-	grunt.initConfig({
-		uglify: {
-			all: ['main.js']
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
-};
